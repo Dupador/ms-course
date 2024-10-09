5d0237ea037c09d791dd3b19610672196cbefbd0
Willaim-dias <williampacifico41@gmail.com>
2024-10-09 08:03:49 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/example/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/service/PaymentService.java
@@ -1,14 +1,33 @@
 package com.example.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.hrpayroll.entities.Payment;
+import com.example.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int day) {
-		return new Payment("Bob",200.0,day);
+		Map<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("id",""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(),worker.getDailyIncome(),day);
 	}
 	
 }
